871c6d69ceff0a56657a19e6d2d798f7d5d860b0
Gideon <eignnx@gmail.com>
2023-12-02 00:20:23 -0500

    add --no-std flag and allow loading multiple files via env::args()

--- repl/src/main.rs
+++ repl/src/main.rs
@@ -12,12 +12,29 @@ fn main() {
 
     println!("Rellog REPL");
 
-    let mut repl = repl::Repl::new_loading_std_lib();
+    let mut repl = repl::Repl::new_loading_files(vec![]);
 
-    if let Some(fname) = std::env::args().nth(1) {
-        let mut tok_buf = Vec::new();
-        let _ = repl.load_file(&mut tok_buf, fname.clone());
-    } else {
+    let args: Vec<_> = std::env::args().skip(1).collect();
+
+    let mut load_std_lib = true;
+    let mut tok_buf = Vec::new();
+
+    for arg in &args {
+        if arg == "--no-std" {
+            load_std_lib = false;
+        } else {
+            let fname = arg;
+            tok_buf.clear();
+            let _ = repl.load_file(&mut tok_buf, fname.clone());
+        }
+    }
+
+    if load_std_lib {
+        tok_buf.clear();
+        let _ = repl.load_file(&mut tok_buf, "./librellog/src/std/std.rellog".into());
+    }
+
+    if args.is_empty() || args == ["--no-std"] {
         println!("Running in interactive mode.");
         println!("Type `:help` for a list of commands.");
         println!("Use `:load` to load a source file.");
