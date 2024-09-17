d02e528d69a1c96b151968aa7710789a922a35c4
Gideon <eignnx@gmail.com>
2024-09-17 14:41:36 -0400

    better src generated

--- pl-backend/src/main.rs
+++ pl-backend/src/main.rs
@@ -1,14 +1,9 @@
-use core::fmt;
-use std::io::Read;
-use std::{char, io};
-use std::{fmt::Formatter, io::Write};
+use std::io::{self, Read, Write};
 
-use librellog::ast::txt::{Segment, TxtErr};
-use librellog::ast::BinOpSymbol;
-use librellog::init_interner;
 use librellog::{
-    ast::{Clause, Item, Module, RcTm, Rel, Tm},
+    ast::{BinOpSymbol, Clause, Item, Module, RcTm, Rel, Tm},
     data_structures::Var,
+    init_interner,
     interner::IStr,
     lex::tok::Tok,
 };
@@ -65,13 +60,31 @@ impl Compile<SwiProlog> for RcTm {
     fn compile(&self, f: &mut dyn Write, compiler: &mut SwiProlog) -> io::Result<()> {
         match &**self {
             Tm::Nil => write!(f, "[]")?,
-            Tm::Cons(head, tail) => {
-                write!(f, "[")?;
-                head.compile(f, compiler)?;
-                write!(f, " | ")?;
-                tail.compile(f, compiler)?;
-                write!(f, "]")?;
-            }
+            Tm::Cons(head, tail) => match self.try_as_list() {
+                Some((init, None)) => {
+                    write!(f, "[")?;
+                    for (i, item) in init.iter().enumerate() {
+                        if i > 0 {
+                            write!(f, ", ")?;
+                        }
+                        item.compile(f, compiler)?;
+                    }
+                    write!(f, "]")?;
+                }
+                Some((init, Some(tail))) => {
+                    write!(f, "[")?;
+                    for (i, item) in init.iter().enumerate() {
+                        if i > 0 {
+                            write!(f, ", ")?;
+                        }
+                        item.compile(f, compiler)?;
+                    }
+                    write!(f, "|")?;
+                    tail.compile(f, compiler)?;
+                    write!(f, "]")?;
+                }
+                None => todo!("non-var non-cons non-nil list tail"),
+            },
             Tm::Rel(rel) => {
                 compiler.compile_rel(f, rel)?;
             }
@@ -86,11 +99,8 @@ impl Compile<SwiProlog> for RcTm {
                     member.compile(f, compiler)?;
                 }
             }
-            Tm::Block(..) => unimplemented!(),
-            Tm::Var(var) => {
-                let mangled = compiler.mangle_var(var);
-                write!(f, "{mangled}")?;
-            }
+            Tm::Block(_other, _) => unimplemented!(),
+            Tm::Var(var) => var.compile(f, compiler)?,
             Tm::Int(i) => write!(f, "{i}")?,
             Tm::TxtCons(..) | Tm::TxtSeg(..) => {
                 let mut buf = String::new();
@@ -134,6 +144,16 @@ impl Compile<SwiProlog> for RcTm {
     }
 }
 
+impl Compile<SwiProlog> for Var {
+    fn compile(&self, f: &mut dyn std::io::Write, _: &mut SwiProlog) -> io::Result<()> {
+        let Var { name, suffix, .. } = self.clone();
+        match suffix {
+            None => write!(f, "{name}"),
+            Some(suffix) => write!(f, "{name}_{suffix}"),
+        }
+    }
+}
+
 impl SwiProlog {
     fn mangle_sig<'a>(&'a self, iter: impl Iterator<Item = &'a IStr>) -> String {
         use std::fmt::Write;
@@ -162,14 +182,6 @@ impl SwiProlog {
             value.compile(f, self)?;
         }
         write!(f, ")")
-    }
-
-    fn mangle_var(&self, var: &Var) -> String {
-        let Var { name, suffix, .. } = var.clone();
-        match suffix {
-            None => format!("{name}"),
-            Some(suffix) => format!("{name}_{suffix}"),
-        }
     }
 }
 
