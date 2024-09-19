f4e98c250fa7246800b028ecc74a8031230aa850
Gideon <eignnx@gmail.com>
2024-09-19 00:33:28 -0400

    better list printing

--- pl-backend/src/tm.rs
+++ pl-backend/src/tm.rs
@@ -1,8 +1,8 @@
 use std::io;
 
 use librellog::{
-    ast::{txt::TxtErr, BinOpSymbol, RcTm, Tm},
-    data_structures::Var,
+    ast::{BinOpSymbol, RcTm, Tm},
+    data_structures::{Sym, Var},
 };
 
 use crate::{Compile, SwiProlog};
@@ -11,35 +11,11 @@ impl Compile<SwiProlog> for RcTm {
     fn compile(&self, f: &mut dyn io::Write, compiler: &mut SwiProlog) -> io::Result<()> {
         match &**self {
             Tm::Nil => write!(f, "[]")?,
-            Tm::Cons(..) => match self.try_as_list() {
-                Some((init, None)) => {
-                    write!(f, "[")?;
-                    for (i, item) in init.iter().enumerate() {
-                        if i > 0 {
-                            write!(f, ", ")?;
-                        }
-                        item.compile(f, compiler)?;
-                    }
-                    write!(f, "]")?;
-                }
-                Some((init, Some(tail))) => {
-                    write!(f, "[")?;
-                    for (i, item) in init.iter().enumerate() {
-                        if i > 0 {
-                            write!(f, ", ")?;
-                        }
-                        item.compile(f, compiler)?;
-                    }
-                    write!(f, "|")?;
-                    tail.compile(f, compiler)?;
-                    write!(f, "]")?;
-                }
-                None => todo!("non-var non-cons non-nil list tail"),
-            },
+            Tm::Cons(..) => compiler.compile_list(f, self)?,
             Tm::Rel(rel) => {
                 compiler.compile_rel(f, rel)?;
             }
-            Tm::Sym(sym) => write!(f, "'{}'", sym)?,
+            Tm::Sym(sym) => sym.compile(f, compiler)?,
             Tm::Block(functor, members) => {
                 writeln!(f, "'$block'('{functor}', [")?;
                 for (i, member) in members.iter().enumerate() {
@@ -101,17 +77,19 @@ impl SwiProlog {
         let mut first = true;
         write!(f, "[")?;
         loop {
-            if !first {
-                write!(f, ", ")?;
-            }
-
             match tail.as_ref() {
                 Tm::Nil => break,
                 Tm::TxtCons(ch, rest) => {
+                    if !first {
+                        write!(f, ", ")?;
+                    }
                     ch.compile(f, self)?;
                     tail = rest;
                 }
                 Tm::TxtSeg(seg) => {
+                    if !first {
+                        write!(f, ", ")?;
+                    }
                     first = true;
                     for ch in seg.segment_as_str().chars() {
                         if !first {
@@ -131,6 +109,46 @@ impl SwiProlog {
             first = false;
         }
         write!(f, "]")?;
+        Ok(())
+    }
+
+    fn compile_list(&mut self, f: &mut dyn io::Write, list: &RcTm) -> io::Result<()> {
+        let mut tail = list;
+        let mut first = true;
+        write!(f, "[")?;
+        loop {
+            match tail.as_ref() {
+                Tm::Nil => break,
+                Tm::Cons(head, rest) => {
+                    if !first {
+                        write!(f, ", ")?;
+                    }
+                    head.compile(f, self)?;
+                    tail = rest;
+                }
+                _ => {
+                    write!(f, " | ")?;
+                    tail.compile(f, self)?;
+                    break;
+                }
+            }
+            first = false;
+        }
+        write!(f, "]")?;
+        Ok(())
+    }
+}
+
+impl Compile<SwiProlog> for Sym {
+    fn compile(&self, f: &mut dyn std::io::Write, _: &mut SwiProlog) -> io::Result<()> {
+        let str = self.to_str();
+        if str.starts_with(|ch: char| ch.is_ascii_lowercase())
+            && !str.contains(|ch: char| matches!(ch, 'a'..='z' | '_' | '0'..='9'))
+        {
+            write!(f, "'{str}'")?;
+        } else {
+            write!(f, "{str}")?;
+        }
         Ok(())
     }
 }
